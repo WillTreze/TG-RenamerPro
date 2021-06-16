1ac96f5808d624a02922e7cb7a0c68c7dbefaf9c
Amani Muhammed <71679579+amanimuhammed@users.noreply.github.com>
2021-06-16 14:05:34 +0530

    Update messages.py

--- mwk/messages.py
+++ mwk/messages.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TEXT = """**Hi {user_mention},\n\nI am a Telegram Renamer & Converter Bot As File/Video with thumbnail support 💯.\n\n𝖥𝗈𝗋 𝖬𝗈𝗋𝖾 𝖣𝖾𝗍𝖺𝗂𝗅𝗌 𝖲𝖾𝖾 /h𝖾𝗅𝗉.\n\n🏷 Maintained By: @Amani_m_h_d**"""
+    START_TEXT = """**Hello ,\n\nI am a Telegram Renamer & Converter Bot As File/Video with thumbnail support 💯.\n\n𝖥𝗈𝗋 𝖬𝗈𝗋𝖾 𝖣𝖾𝗍𝖺𝗂𝗅𝗌 𝖲𝖾𝖾 /h𝖾𝗅𝗉.\n\n🏷 Maintained By: @Amani_m_h_d**"""
 ######################
     HELP_USER = """**Hey, Follow Below Steps:**\n\n☞ **Send a photo to set as thumbnail.**\n☞︎︎︎ **Now send me the Telegram file or video you want to rename & convert.**\n☞︎︎︎ **Select desired option And done wait for it to process files.**\n\n**🏷 Maintained By: @Amani_m_h_d**"""
     DOWNLOAD_MSG = "**Yup Bro, Downloading....**😌"
