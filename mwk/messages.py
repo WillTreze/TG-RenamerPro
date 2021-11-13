9ec7c221814c697279bf99d3ef474dc43fe0a0d3
Will <48420466+WillTreze@users.noreply.github.com>
2021-11-13 15:26:55 -0300

    Update messages.py

--- mwk/messages.py
+++ mwk/messages.py
@@ -1,7 +1,7 @@
 class Translation(object):
-    START_TEXT = """**Hello ,\n\nI am a Telegram Renamer & Converter Bot As File/Video with thumbnail support 💯.\n\n𝖥𝗈𝗋 𝖬𝗈𝗋𝖾 𝖣𝖾𝗍𝖺𝗂𝗅𝗌 𝖲𝖾𝖾 /help.**\n**"""
+    START_TEXT = """**Hello ,\n\nI am a Telegram Renamer & Converter Bot As File/Video with thumbnail support 💯.\n\n𝖥𝗈𝗋 𝖬𝗈𝗋𝖾 𝖣𝖾𝗍𝖺𝗂𝗅𝗌 𝖲𝖾𝖾 /help.**\n"""
 ######################
-    HELP_USER = """**Hey, Follow Below Steps:**\n\n☞ **Send a photo to set as thumbnail.**\n☞︎︎︎ **Now send me the Telegram file or video you want to rename & convert.**\n☞︎︎︎ **Select desired option And done wait for it to process files.**\n\n***"""
+    HELP_USER = """**Hey, Follow Below Steps:**\n\n☞ **Send a photo to set as thumbnail.**\n☞︎︎︎ **Now send me the Telegram file or video you want to rename & convert.**\n☞︎︎︎ **Select desired option And done wait for it to process files.**\n"""
     DOWNLOAD_MSG = "**Yup Bro, Downloading....**😌"
     DOWNLOAD_FAIL_MSG = "**Sorry Baby 😔, Something Error With Your File**❎"
     UPLOAD_MSG = "**Yeah Bro, Uploading it**🤓"
